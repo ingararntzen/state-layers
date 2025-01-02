378df93fa94e3c3d683976b1a38c29a0dc434a9b
ingarantzen <ingar.arntzen@gmail.com>
2025-01-02 19:38:44 +0100

    added web builds in html/js

---
+++ html/js/layers.es.js
@@ -0,0 +1,6 @@
+// Use export keyword to export functions or variables
+function sum(a, b) {
+    return a + b;
+}
+
+export { sum };
---
+++ html/js/layers.iife.js
@@ -0,0 +1,14 @@
+var LAYERS = (function (exports) {
+    'use strict';
+
+    // Use export keyword to export functions or variables
+    function sum(a, b) {
+        return a + b;
+    }
+
+    exports.sum = sum;
+
+    return exports;
+
+})({});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibGF5ZXJzLmlpZmUuanMiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9zdW0uanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gVXNlIGV4cG9ydCBrZXl3b3JkIHRvIGV4cG9ydCBmdW5jdGlvbnMgb3IgdmFyaWFibGVzXG5leHBvcnQgZnVuY3Rpb24gc3VtKGEsIGIpIHtcbiAgICByZXR1cm4gYSArIGI7XG59Il0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztJQUFBO0lBQ08sU0FBUyxHQUFHLENBQUMsQ0FBQyxFQUFFLENBQUMsRUFBRTtJQUMxQixJQUFJLE9BQU8sQ0FBQyxHQUFHLENBQUM7SUFDaEI7Ozs7Ozs7Ozs7In0=
