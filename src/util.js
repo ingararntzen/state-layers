6910ffc3bfc6d806f33e9bd1bd2a0d0bc2c41f0c
ingarantzen <ingar.arntzen@gmail.com>
2025-01-08 21:27:58 +0100

    started working with cursors

---
+++ src/util.js
@@ -0,0 +1,3 @@
+
+// clock - based on performance now - in seconds
+export function clock() {return performance.now()/1000.0; }
---
+++ src/variable.js
@@ -0,0 +1,15 @@
+import { IndexCursor } from './cursor_base.js';
+import { clock } from './util.js'
+
+/**
+ * Real-Time Variable
+ */
+
+class RealTImeVariable extends IndexCursor {
+
+    constructor(index) {
+        this._items = [];  
+    }
+
+
+}
